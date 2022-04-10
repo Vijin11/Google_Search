529a4efe54097e81fa974dd7b69c03128c897a0e
Vijin11 <vijins30@gmail.com>
2022-04-10 15:46:19 +0530

    Test Elements added

---
+++ Google Search Pjct/Test Suites/GoogleSearch1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GoogleSearch1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7f7372bc-064c-4183-accb-f4e364b71c8f</testSuiteGuid>
+   <testCaseLink>
+      <guid>d083d19b-ad38-487e-bd20-b65d2b2c300b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
